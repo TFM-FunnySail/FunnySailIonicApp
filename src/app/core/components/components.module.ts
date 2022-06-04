eec6f671db6ea3909e43904594f5cd04f4f68779
Diego Juarez Romero <diegojuarez_13@hotmail.com>
2022-06-04 17:07:32 +0200

    ADD: basic footer

--- src/app/core/components/components.module.ts
+++ src/app/core/components/components.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FooterModule } from './footer/footer.module';
 import { HeaderModule } from "./header/header.module";
 
 @NgModule({
   imports: [
     HeaderModule,
+    FooterModule
   ],
   exports: [
     HeaderModule,
+    FooterModule
   ]
 })
 export class ComponentsModule {
