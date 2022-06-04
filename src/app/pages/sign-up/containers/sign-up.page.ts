b525c1a4624e2af17a9a21eb89fe6b0022f15354
Orlando Marques <realorlanditomadrid@gmail.com>
2022-06-04 19:49:17 +0200

    ADD: credentials and user data form to sign-up page

--- src/app/pages/sign-up/containers/sign-up.page.ts
+++ src/app/pages/sign-up/containers/sign-up.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {UsersService} from "../../../shared/sdk";
+import { format, parseISO, getDate, getMonth, getYear } from 'date-fns';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpPage implements OnInit {
 
-  constructor() { }
+  public segment:string = "identity";
+  registerForm: FormGroup;
+  dateValue2: any;
+
+  constructor(private router: Router,
+              private formBuilder: FormBuilder,
+              protected userService: UsersService,) {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9_@./#&+-]{8,25}$')]],
+      confirmPassword: ['', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9_@./#&+-]{8,25}$')]],
+      lastName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      birthDay: ['', [Validators.required,]],
+      phoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      Address: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      City: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      State: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      ZipCode: ['', [Validators.required, Validators.pattern('^[0-9]{5}$')]],
+      Country: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      });
+  }
 
   ngOnInit() {
   }
 
+  formatDate(value: string) {
+    return format(parseISO(value), 'MMM dd yyyy');
+  }
+
+  reFormatDate(value: string) {
+    const date = new Date(value);
+    return date.toISOString();
+  }
+
+  segmentChanged($event: any) {
+    this.segment = $event.detail.value;
+    console.log(this.segment);
+  }
+
+  onSubmit() {
+
+    if(!this.registerForm.invalid){
+      console.log(this.registerForm.value);
+      this.registerForm.value.birthDay = this.reFormatDate(this.registerForm.value.birthDay);
+      this.userService.apiUsersPost(this.registerForm.value).subscribe(()=>{
+        this.router.navigate(['']);
+      }, (error) => {
+        console.log(error);
+      });
+    }
+  }
 }
--- src/app/pages/sign-up/sign-up.module.ts
+++ src/app/pages/sign-up/sign-up.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { SignUpPageRoutingModule } from './sign-up-routing.module';
 
 import { SignUpPage } from './containers/sign-up.page';
+import {CoreModule} from "../../core/core.module";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
+    CoreModule,
     SignUpPageRoutingModule
   ],
   declarations: [SignUpPage]
