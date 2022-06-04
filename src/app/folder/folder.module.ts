61e6b84988ab3f4cc672133225e810efb4e7a117
Diego Juarez Romero <diegojuarez_13@hotmail.com>
2022-06-04 14:42:57 +0200

    ADD: core module with header component

--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { FolderPageRoutingModule } from './folder-routing.module';
 
 import { FolderPage } from './folder.page';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    FolderPageRoutingModule,
+    CoreModule
   ],
   declarations: [FolderPage]
 })
