c3e194aec77ea2e7c96c9c4cf1af505d7a50d039
Orlando Marques <realorlanditomadrid@gmail.com>
2022-06-05 12:13:32 +0200

    ADD: booking-cart service to service detail page

--- src/app/pages/service-detail/containers/service-detail.page.ts
+++ src/app/pages/service-detail/containers/service-detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {ServiceOutputDTO, ServicesService} from "../../../shared/sdk";
 import {StorageService} from "../../../shared/services/storage/storage.service";
+import {BookingCartService} from "../../../shared/services/booking-cart/booking-cart.service";
 
 @Component({
   selector: 'app-service-detail',
@@ -16,7 +17,8 @@ export class ServiceDetailPage implements OnInit {
   constructor(private activatedRoute: ActivatedRoute,
               private servicesService: ServicesService,
               private storageService: StorageService,
-              private router: Router) {
+              private router: Router,
+              protected bookingCartService:BookingCartService) {
     this.service = {
       id: -1,
       name: '',
@@ -41,33 +43,9 @@ export class ServiceDetailPage implements OnInit {
   booking(){
     const id = this.service.id;
     if (id) {
-      const bookingCart = this.storageService.getItem('bookingCart');
-      let bookingCartJSON:any;
-      if (bookingCart) {
-        bookingCartJSON = JSON.parse(bookingCart);
-        if (bookingCartJSON.services) {
-          if(!this.existService(bookingCartJSON, id)){
-            bookingCartJSON.services.push({id});
-          }
-        } else {
-          bookingCartJSON.services = [{id}];
-          console.log(bookingCartJSON);
-        }
-      } else {
-        bookingCartJSON = {services: [{id}]};
-      }
-      this.storageService.setItem('bookingCart', JSON.stringify(bookingCartJSON));
-      this.router.navigate(['/booking']);
+      this.bookingCartService.addService(this.service);
+      this.router.navigate(['services']);
     }
-  }
-
-  private existService(bookingCartJSON: any, id: number) {
-    for (let ser of bookingCartJSON.services) {
-      if (parseInt(ser.id) === id) {
-        return true;
-      }
-    }
-    return false;
   }
 
 }
