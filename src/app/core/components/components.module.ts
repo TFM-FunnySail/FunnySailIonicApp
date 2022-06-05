569c95dff1f94d416e7e7a9d80db4e8af037298d
Diego Juarez Romero <diegojuarez_13@hotmail.com>
2022-06-05 14:46:34 +0200

    ADD: comment component

--- src/app/core/components/components.module.ts
+++ src/app/core/components/components.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CardMenuModule } from './card-menu/card-menu.module';
+import { CommentModule } from './comment/comment.module';
 import { FooterModule } from './footer/footer.module';
 import { HeaderModule } from "./header/header.module";
 
@@ -8,11 +9,13 @@ import { HeaderModule } from "./header/header.module";
     HeaderModule,
     FooterModule,
     CardMenuModule,
+    CommentModule,
   ],
   exports: [
     HeaderModule,
     FooterModule,
     CardMenuModule,
+    CommentModule,
   ]
 })
 export class ComponentsModule {
