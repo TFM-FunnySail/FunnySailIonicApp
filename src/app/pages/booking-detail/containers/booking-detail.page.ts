720bb052e8539fb1ffc8f164e2ae397df3d46490
Orlando Marques <realorlanditomadrid@gmail.com>
2022-06-07 11:46:59 +0200

    ADD: cancelled button to booking-detail page

--- src/app/pages/booking-detail/containers/booking-detail.page.ts
+++ src/app/pages/booking-detail/containers/booking-detail.page.ts
@@ -3,6 +3,7 @@ import {BookingOutputDTO, BookingService} from "../../../shared/sdk";
 import {ActivatedRoute} from "@angular/router";
 import {BoatBookingCartModel} from "../../../shared/data/booking-cart";
 import {format, parseISO} from "date-fns";
+import {AlertController} from "@ionic/angular";
 
 @Component({
   selector: 'app-booking-detail',
@@ -15,8 +16,11 @@ export class BookingDetailPage implements OnInit {
   booking?: BookingOutputDTO | null = {};
   boatsBooking: BoatBookingCartModel[] = [];
   totalAmount: number = 0;
+  canBeCanceled: boolean = false;
+  canBePaid: boolean = false;
   constructor(protected bookingService: BookingService,
-              protected router: ActivatedRoute) { }
+              protected router: ActivatedRoute,
+              protected alertController:AlertController) { }
 
   ngOnInit() {
     this.router.params.subscribe(params => {
@@ -43,7 +47,7 @@ export class BookingDetailPage implements OnInit {
       }
     });
     this.totalAmount = resp.clientInvoiceLine.totalAmount;
-
+    this.handlerStatus(resp);
     return resp;
   }
 
@@ -52,5 +56,56 @@ export class BookingDetailPage implements OnInit {
       return '';
 
     return format(parseISO(value), 'MMM dd yyyy');
+  }
+
+  async cancel() {
+    await this.cancelAlertConfirm();
+  }
+
+  async pay() {
+
+  }
+
+  async cancelAlertConfirm() {
+    const alert = await this.alertController.create({
+      cssClass: '',
+      header: 'Confirmar',
+      message: '¿Está seguro que desea <strong>cancelar</strong> la orden?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          cssClass: 'secondary',
+          id: 'cancel-button',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Si',
+          id: 'confirm-button',
+          handler: () => {
+            console.log('Confirm Okay');
+            this.cancelOrder();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  private cancelOrder() {
+    this.bookingService.apiBookingIdCancelPut(this.id).subscribe(resp=>{
+      console.log(resp);
+      this.booking.status = 'Cancelled';
+      this.handlerStatus(this.booking);
+    },err=>{
+      console.log(err);
+    })
+  }
+
+  private handlerStatus(resp: BookingOutputDTO) {
+    this.canBeCanceled = resp.status !== 'Cancelled' && resp.status !== 'Completed';
+    this.canBePaid = resp.status !== 'Cancelled' && !resp.paid;
   }
 }
