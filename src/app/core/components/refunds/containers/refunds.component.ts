a226754d6dd415d7364af50bd834578f1e6661a5
Diego Juarez Romero <diegojuarez_13@hotmail.com>
2022-06-07 16:48:26 +0200

    ADD: some changes in refund component

--- src/app/core/components/refunds/containers/refunds.component.ts
+++ src/app/core/components/refunds/containers/refunds.component.ts
@@ -9,7 +9,7 @@ import { RefundOutputDTO } from "../../../../shared/sdk";
 export class RefundsComponent implements OnInit {
 
   @Input() refund: RefundOutputDTO = {};
-  name: string = '';
+  name: string = 'Devolucion ';
   createdDate: string = '';
   description: string = '';
   amountToReturn: number = 0;
@@ -18,7 +18,7 @@ export class RefundsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.name = this.refund.id as unknown as string;
+    this.name += this.refund.id as unknown as string;
     this.createdDate = this.refund.date as string;
     this.description = this.refund.description as string;
     this.amountToReturn = this.refund.amountToReturn as number;
