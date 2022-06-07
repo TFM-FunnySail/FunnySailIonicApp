c76c45595f89a5c54ac37bfcf6ad55cecf96febe
andresll706 <andresll706@gmail.com>
2022-06-08 01:30:49 +0200

    ADD: translate boat-card component

--- src/app/core/components/boat-card/boat-card.module.ts
+++ src/app/core/components/boat-card/boat-card.module.ts
@@ -2,15 +2,17 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { RouterModule } from "@angular/router";
 import {BoatCardComponent} from "./containers/boat-card.component";
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
   declarations: [
     BoatCardComponent
   ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        TranslateModule
+    ],
   exports: [
     BoatCardComponent
   ]
